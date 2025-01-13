e052050b32cd57c56083e5a737cff25068460f96
Jtility <leejewon97@gmail.com>
2025-01-13 18:22:27 +0900

    의상 개수만 알아도 된다는 점과 조합론 곱의 법칙 사용

--- week2/Clothes.kt
+++ week2/Clothes.kt
@@ -1,34 +1,15 @@
 class Solution {
     fun solution(clothes: Array<Array<String>>): Int {
-        var answer = 0
-        val map = HashMap<String, String>()
-        clothes.forEach { map[it[1]] = it[0] }
-        repeat(map.size) { answer += combInHash(clothes, it + 1) }
-        return answer
-    }
-    val comb = mutableListOf<Array<String>>()
-    val combToMaps = mutableListOf(mapOf<String, String>())
-
-    fun combClothes(clothes: Array<Array<String>>, num: Int, curr: Int) {
-        if (comb.size == num) {
-            // [1]을 key로 map에 넣음
-            combToMaps.add(comb.map { it[1] to it[0] }.toMap())
-            return
+        var answer = 1
+        // 종류별 의상 개수만으로 이루어진 map
+        val map = mutableMapOf<String, Int>()
+        clothes.forEach {
+            map[it[1]] = map.getOrDefault(it[1], 0) + 1 // 키가 있으면 가져오고, 없으면 2번째 인자
+            // 결국 키값(종류)이 동일하면 개수를 1 증가시켜 개수를 카운트
         }
-        for (i in curr until clothes.size) {
-            comb.add(clothes[i])
-            combClothes(clothes, num, i + 1)
-            comb.removeLast()
-        }
-    }
 
-    fun combInHash(clothes: Array<Array<String>>, num: Int): Int {
-        var comb = 0
-        // clothes 중 중복되지 않게 num개 고름
-        combClothes(clothes, num, 0)
-        // 길이가 num인지 확인
-        combToMaps.forEach { if (it.size == num) comb += 1 }
-        combToMaps.clear()
-        return comb
+        // 조합론 곱의 법칙
+        map.forEach { answer *= (it.value + 1) }
+        return answer - 1
     }
 }
