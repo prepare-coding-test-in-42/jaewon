cc993cc4c8441967fd4914dcef70614c46ca3b2f
Jtility <leejewon97@gmail.com>
2025-01-13 18:29:52 +0900

    groupBy, mapValues, fold를 활용하여 코드 단순화

--- week2/Clothes.kt
+++ week2/Clothes.kt
@@ -1,15 +1,11 @@
 class Solution {
     fun solution(clothes: Array<Array<String>>): Int {
-        var answer = 1
+        var answer = 0
         // 종류별 의상 개수만으로 이루어진 map
-        val map = mutableMapOf<String, Int>()
-        clothes.forEach {
-            map[it[1]] = map.getOrDefault(it[1], 0) + 1 // 키가 있으면 가져오고, 없으면 2번째 인자
-            // 결국 키값(종류)이 동일하면 개수를 1 증가시켜 개수를 카운트
-        }
+        val map = clothes.groupBy { it[1] }.mapValues { it.value.size }
 
         // 조합론 곱의 법칙
-        map.forEach { answer *= (it.value + 1) }
-        return answer - 1
+        answer = map.values.fold(1) { acc, i -> acc * (i + 1) } - 1
+        return answer
     }
 }
