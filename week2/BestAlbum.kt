b5a2858d3cf183c4994f9656bd60c2967241e6f3
Jtility <leejewon97@gmail.com>
2025-01-13 21:35:23 +0900

    BestAlbum

---
+++ week2/BestAlbum.kt
@@ -0,0 +1,24 @@
+class Solution {
+    fun solution(genres: Array<String>, plays: IntArray): IntArray {
+        var answer = mutableListOf<Int>()
+        // 필요정보 : 종류, 인덱스, 값
+        val map = mutableMapOf<String, MutableMap<Int, Int>>()
+        for (i in plays.indices) {
+            if (map[genres[i]].isNullOrEmpty()) {
+                map[genres[i]] = mutableMapOf(i to plays[i])
+            } else {
+                map[genres[i]]!![i] = plays[i]
+            }
+        }
+        val mapToSortedList = map.toList().sortedByDescending { it.second.values.sum() }
+        // [(pop, {1=600, 4=2500}), (classic, {0=500, 2=150, 3=800})]
+        mapToSortedList.forEach {
+            val playList = it.second.toList().sortedByDescending { play -> play.second }
+            answer.add(playList.first().first)
+            if (playList.size >= 2) {
+                answer.add(playList[1].first)
+            }
+        }
+        return answer.toIntArray()
+    }
+}
