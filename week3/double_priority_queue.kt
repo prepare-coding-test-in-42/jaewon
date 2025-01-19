92f19c99a161a0a2634f9f13e00b0b7f2c300a86
Jtility <leejewon97@gmail.com>
2025-01-20 00:56:05 +0900

    가독성 개선

--- week3/double_priority_queue.kt
+++ week3/double_priority_queue.kt
@@ -2,12 +2,12 @@ class Solution {
     fun solution(operations: Array<String>): IntArray {
         val queue = mutableListOf<Int>()
         operations.forEach {
-            val list = it.split(" ")
-            when {
-                list[0] == "I" -> queue.add(list[1].toInt())
-                else -> {
-                    if (list[1].toInt() == 1)  queue.remove(queue.maxOrNull())
-                    else queue.remove(queue.minOrNull())
+            val (cmd, data) = it.split(" ")
+            when (cmd) {
+                "I" -> queue.add(data.toInt())
+                else -> when (data) {
+                    "1" -> queue.remove(queue.maxOrNull())
+                    else -> queue.remove(queue.minOrNull())
                 }
             }
         }
