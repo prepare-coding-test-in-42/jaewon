4e9515929e91cf830ff50ae6f06039170d44979e
Jtility <leejewon97@gmail.com>
2025-07-09 20:52:54 +0900

    time out whack-a-mole

--- july_week2/whack-a-mole.kt
+++ july_week2/whack-a-mole.kt
@@ -5,13 +5,8 @@ import kotlin.math.*
 
 fun main() {
     val (n, s) = readln().split(" ").map { it.toInt() }
-    val moles = PriorityQueue<Triple<Int, Int, Int>> { a, b ->
-        a.third.compareTo(b.third)
-    }
-    repeat(n) {
-        readln().split(" ").map { it.toInt() }.let { (x, y, t) ->
-            moles.offer(Triple(x, y, t))
-        }
+    val moles = Array(n) {
+        readln().split(" ").map { it.toInt() }
     }
 
     fun calcDist(jx: Int, jy: Int, mx: Int, my: Int): Double {
@@ -19,4 +14,50 @@ fun main() {
         val dy = (jy - my).toDouble()
         return sqrt(dx.pow(2) + dy.pow(2))
     }
+
+    val dp = Array(n) {
+        BooleanArray(n)
+    }
+    for (i in 0 until n) {
+        val (x, y, t) = moles[i]
+        val time = calcDist(0, 0, x, y) / s
+        if (t >= time) {
+            dp[0][i] = true
+        }
+    }
+    for (i in 0 until n) {
+        for (j in 0 until n) {
+            if (i != j) {
+                val (ix, iy, it) = moles[i]
+                val (jx, jy, jt) = moles[j]
+                val time = calcDist(ix, iy, jx, jy) / s
+                if (jt - it >= time) {
+                    dp[i][j] = true
+                }
+            }
+        }
+    }
+
+    var maxCount = 0
+    val visited = BooleanArray(n)
+    fun dfs(current: Int, count: Int) {
+        maxCount = maxOf(maxCount, count)
+        
+        for (next in 0 until n) {
+            if (!visited[next] && dp[current][next]) {
+                visited[next] = true
+                dfs(next, count + 1)
+                visited[next] = false
+            }
+        }
+    }
+    
+    for (start in 0 until n) {
+        if (dp[0][start]) {
+            visited[start] = true
+            dfs(start, 1)
+            visited[start] = false
+        }
+    }
+    println(maxCount)
 }
